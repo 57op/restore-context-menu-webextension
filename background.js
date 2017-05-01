d0b1d129124ca7dafaf67a12aae830eb703fee23
chuck- <carlogreco93@gmail.com>
2017-05-01 15:27:17 +0200

    reset context menu on tabs macthing URLs

--- background.js
+++ background.js
@@ -3,20 +3,33 @@ function onOptionsReadyOrUpdate(result/*, areaName = "local"*/) {
 		return;
 	}
 	
+	var urls = result.urls.newValue || result.urls;
+
 	browser.webRequest.onCompleted.removeListener(resetContextMenu);
 	browser.webRequest.onCompleted.addListener(
 		resetContextMenu,
-		{ urls: result.urls.newValue || result.urls }
+		{ urls: urls }
 	);
+	
+	// instantly apply script on matching tabs
+	browser.tabs.query({
+		url: urls
+	}).then(resetContextMenuTabs);
+}
+
+function resetContextMenuTabs(tabs) {
+	tabs.forEach(resetContextMenu);
 }
 
 function resetContextMenu(details) {
 	// console.log(`[resetContextMenu] Executing script at ${details.url}`);*/
 	browser.tabs.executeScript(
-		details.tabId,
+		details.id || details.tabId,
 		{ code: "document.oncontextmenu = null", runAt: "document_end" }
 	);
 }
 
 browser.storage.local.get("urls").then(onOptionsReadyOrUpdate);
 browser.storage.onChanged.addListener(onOptionsReadyOrUpdate);
+
+console.log('[rs] Running');
